d0b6b7a01c92ea2a8d68fb8d06015bd5097daa0c
QuangVann <115283300+QuangVann@users.noreply.github.com>
2023-06-21 23:45:43 +0700

    debug

--- public/main/toanhoc.js
+++ public/main/toanhoc.js
@@ -1,52 +1,4 @@
-// <![CDATA[  <-- For SVG support
-if ("WebSocket" in window) {
-  (function () {
-    function refreshCSS() {
-      var sheets = Array.from(document.getElementsByTagName("link"));
-      var head = document.getElementsByTagName("head")[0];
-      sheets.forEach(function (elem) {
-        var parent = elem.parentElement || head;
-        parent.removeChild(elem);
-        var rel = elem.rel;
-        if (
-          (elem.href && typeof rel !== "string") ||
-          rel.length === 0 ||
-          rel.toLowerCase() === "stylesheet"
-        ) {
-          var url = elem.href.replace(/(&|\?)_cacheOverride=\d+/, "");
-          elem.href =
-            url +
-            (url.indexOf("?") >= 0 ? "&" : "?") +
-            "_cacheOverride=" +
-            new Date().valueOf();
-        }
-        parent.appendChild(elem);
-      });
-    }
 
-    var protocol = window.location.protocol === "http:" ? "ws://" : "wss://";
-    var address =
-      protocol + window.location.host + window.location.pathname + "/ws";
-    var socket = new WebSocket(address);
-    socket.onmessage = function (msg) {
-      if (msg.data === "reload") window.location.reload();
-      else if (msg.data === "refreshcss") refreshCSS();
-    };
-    if (
-      sessionStorage &&
-      !sessionStorage.getItem("IsThisFirstTime_Log_From_LiveServer")
-    ) {
-      console.log("Live reload enabled.");
-      sessionStorage.setItem("IsThisFirstTime_Log_From_LiveServer", true);
-    }
-  })();
-} else {
-  console.error(
-    "Upgrade your browser. This Browser does NOT support WebSocket for Live-Reloading."
-  );
-}
-
-// ]]>
 
 $("#goToMap").on("click", function (event) {
   window.open("https://maps.app.goo.gl/arAnxNQZ9BjNMHXf7?g_st=ic", "_blank");
@@ -131,7 +83,7 @@ $(".submitButton").on("click", function (event) {
     });
   } else {
     var src = window.location.href;
-    submitStudent(name, "null", phone, true, false, src);
+    submitStudent(name, grade, phone, true, false, src);
   }
 });
 // end_form
